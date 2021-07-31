ad2a64ad9c42b0a0c1facbc90a1861d98f49954d
Nathan Hellinga <nathaniel.hellinga@gmail.com>
2021-07-30 20:23:54 -0400

    reduce hydroponics cost so you dont end up stranded

--- src/GameData/colonyObjects.js
+++ src/GameData/colonyObjects.js
@@ -2,7 +2,7 @@ export const colonyObjects = {
   hydroponics: {
     title: "Hydroponics Lab",
     subtitle: "A hydroponics lab will allow us to start growing our own plant matter. Its not tasty but at least we wont starve.",
-    basePrice: 5000,
+    basePrice: 500,
     costMultiFactor: 2,
     workers: 3,
     inputs: [],
